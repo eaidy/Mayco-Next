f72fda7d6bc6f02b6cbdd61a164bf3a531677074
Ata Can Çalışkan <64282143+eaidy@users.noreply.github.com>
2022-10-10 17:28:43 +0300

    ...

--- components/CorporateNav.js
+++ components/CorporateNav.js
@@ -1,7 +1,10 @@
 import { useState } from "react";
+import { useRouter } from "next/router";
 import Link from "next/link";
 
 const CorporateNav = ({ corporateObject }) => {
+
+    const router = useRouter()
 
     const [lang, ] = useState(() => corporateObject.lang === "en" ? "en/" : "")
 
@@ -13,7 +16,12 @@ const CorporateNav = ({ corporateObject }) => {
                         <Link key={index} href={'/' + lang + corporateObject.site + "/corporate/" + nav.path}>
                             <a className="corporate-nav d-flex justify-content-between align-items-center border-bottom py-4">
                                 { nav.label }
-                                <i className="fas fa-chevron-right chevron-icon"></i>
+                                {
+                                    router.pathname.includes(nav.path) &&
+                                    (
+                                        <i className="fas fa-chevron-right chevron-icon"></i>
+                                    )
+                                }
                             </a>
                         </Link>
                     ))
