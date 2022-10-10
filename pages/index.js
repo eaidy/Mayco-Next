bac97d24e5ae7f9af8cf50cff621137e54e293e4
Ata Can Çalışkan <64282143+eaidy@users.noreply.github.com>
2022-10-10 15:09:46 +0300

    ...

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Image from 'next/image'
 import Link from 'next/link'
 
 export default function Home() {
@@ -7,17 +8,23 @@ export default function Home() {
     <div className="index-wrapper">
       <div className="container-fluid">
         <div className="row">
-          <div className="col-md-6 col-sm-12 text-center site-link site-link-textile">
+          <div className="col-md-6 cd-flex align-items-center justify-content-center text-center site-link site-link-textile">
             <div className="index-icon">
               <h2>
-                <Link href="/textile"><a className="site-route">MAYCO TEXTILE</a></Link>
+                <Link href="/textile">
+                  <Image src="/logos/mayco-textile-1.svg" alt="..." width={250} height={250}/>
+                </Link>
+                <Link href="/textile"><a className="site-route d-block">MAYCO TEXTILE</a></Link>
               </h2>
             </div>
           </div>
-          <div className="col-md-6 col-sm-12 text-center site-link site-link-marine">
+          <div className="col-md-6 d-flex align-items-center justify-content-center text-center site-link site-link-marine">
             <div className="index-icon">
               <h2>
-                <Link href="/marine"><a className="site-route">MAYCO MARINE</a></Link>
+                <Link href="/marine">
+                  <Image src="/logos/mayco-marine-1.svg" alt="..." width={250} height={250}/>
+                </Link>
+                <Link href="/marine"><a className="site-route d-block">MAYCO MARINE</a></Link>
               </h2>
             </div>
           </div>
