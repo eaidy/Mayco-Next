e2fc024c8b283b0e15b34ed8d3e5b2b08bbba6e8
Ata Can Çalışkan <caliskanataa@gmail.com>
2023-02-23 17:40:05 +0300

    ...

--- pages/textile/corporate/index.js
+++ pages/textile/corporate/index.js
@@ -6,6 +6,7 @@ import CorporateNav from "../../../components/CorporateNav";
 
 const OurMission = () => {
     return (
+        
         <>
             <Head>
                 <title>MAYCO | Kurumsal</title>
